23b81e03bd51fdd32b4a432f9998472f05ebea2c
DingC <1017472599@qq.com>
2020-09-25 15:25:50 +0800

    resolve 4.4

--- ch4_desicion_tree/4.4_CART/src/CART.py
+++ ch4_desicion_tree/4.4_CART/src/CART.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-
+from pydotplus import graphviz
 
 class Node:
     '''
@@ -30,14 +30,13 @@ def loadDataSet(file_name):
     return df
 
 
-def GetLabelCount(df):
+def GetLabelCount(label_arr):
     '''
     :param df: the pandas dataframe of the dataset
     :return: dist:{key, value}
              key: label
              value: label count
     '''
-    label_arr = df[df.columns[-1]]
     label_count = {}
     for label in label_arr:
         if label in label_count:
@@ -52,51 +51,39 @@ def SameAttrValue(df):
     :param df: the pandas dataframe of the dataset
     :return: True if all row has the same attribute value, otherwise False
     '''
-    for i in len(df):
+    for i in range(len(df)):
         if not (df.iloc[0, 1:-1] == df.iloc[i, 1:-1]).all():
             return False
     return True
 
 
-def InfoEnt(label_arr):
-    '''
-    :param label_arr: the label array of the dataset
-    :return: the information entropy of the dataset
-    '''
-    ent = 0
-    n = len(label_arr)
+def Gini(label_arr):
     label_count = GetLabelCount(label_arr)
+    p_same = 0
+    n = len(label_arr)
     for label in label_count:
-        ent -= (label_count[label] / n) * np.log2(label_count[label] / n)
-    return ent
+        p_same += np.square(label_count[label] / n)
+    return 1 - p_same
 
 
-def InfoGain(df, attr):
-    '''
-    :param df: the pandas dataframe of the dataset
-    :param attr: the specify attribute
-    :return: information gain of df divided by attribute
-    '''
-    info_gain = InfoEnt(df[df.columns[-1]])
+def GiniIndex(df, attr):
     n = len(df[attr])
-    if df[attr].dtype == (float, int):
-        sub_info_ent = {}
-        df = df.sort([attr], asending=1)
-        value_arr = df[attr]
-        label_arr = df[df.columns[-1]]
-        for i in range(n):
-            div = (value_arr[i] + value_arr[i+1]) / 2
-            sub_info_ent[div] = ((i+1) / n * InfoEnt(label_arr[0:i+1]) \
-                                 + (n-i-1) / n * InfoEnt(label_arr[i+1:-1]))
-        div_value, min_sub_info_ent = min(sub_info_ent.items(), key=lambda x:x[1])
-        info_gain -= min_sub_info_ent
+    gini_index = 0
+    div_value = 0
+    if df[attr].dtype == float:
+        sub_gini_index = {}
+        for i in range(n-1):
+            div = (df[attr][i] + df[attr][i+1]) / 2
+            sub_gini_index[div] = ((i+1) * Gini(df[df.columns[-1]][0:i+1]) / n) \
+                                    + ((n-i-1) * Gini(df[df.columns[-1]][i+1:-1]) / n)
+        div_value, gini_index = min(sub_gini_index.items(), key=lambda x:x[1])
     else:
         value_count = GetValueCount(df[attr])
         for value in value_count:
-            df_v = df[df[attr].isin[value]]
-            info_gain -= value_count[value] / len(df[attr]) * InfoEnt(df_v[df.columns[-1]])
-
-    return info_gain, div_value
+           df_v = df[df[attr].isin([value])]
+           gini_index += value_count[value] * Gini(df_v[df_v.columns[-1]]) / n
+           print('value: ' + str(value) + ' value_count: ' + str(value_count[value]) + ' gini_index: ' + str(gini_index))
+    return gini_index, div_value
 
 
 def SelectOptAttr(df):
@@ -104,15 +91,17 @@ def SelectOptAttr(df):
     :param df: the pandas dataframe of the dataset
     :return: (the optimal attribute, the optimal divide value)
     '''
-    gain = 0
+    min_gini = float('Inf')
+    print(df)
     # for each attribute, compute info gain, and find the attribute which has maximum info gain
     for attr in df.columns[1:-1]:
         # compute info gain
-        gain_tmp, div_tmp = InfoGain(df, attr)
-        if gain_tmp > gain:
-            gain = gain_tmp
+        gini_index, div_value = GiniIndex(df, attr)
+        print('attr: ' + str(attr) + ' gini_index: ' + str(gini_index) + ' div_value: ' + str(div_value))
+        if gini_index < min_gini:
+            min_gini = gini_index
             opt_attr = attr
-            div_val = div_tmp
+            div_val = div_value
     return opt_attr, div_val
 
 
@@ -131,18 +120,19 @@ def GetValueCount(value_arr):
             value_count[value] = 1
     return value_count
 
+
 def TreeGenerate(df):
     # generate node
     node = Node(None, None, {})
     # if samples in set D are the same class C, set leaf node the Class C and return
     label_count = GetLabelCount(df[df.columns[-1]])
     if len(label_count) == 1:
-        node.label = max(label_count, key=label_count.get())
+        node.label = max(label_count, key=label_count.get)
         return node
     # if attribute set A is NULL or the samples in D are the same value on attribute A,
     # set leaf node the class which has the most samples on set D
     if len(df.columns[1:-1]) == 0 or SameAttrValue(df):
-        node.label = max(label_count, key=label_count.get())
+        node.label = max(label_count, key=label_count.get)
         return node
     # select optimal divide attribute
     opt_attr, div_val = SelectOptAttr(df)
@@ -152,10 +142,10 @@ def TreeGenerate(df):
         value_count = GetValueCount(df[opt_attr])
         for v in value_count:
             # get D_v by v
-            df_v = df[df[opt_attr].isin[v]]
+            df_v = df[df[opt_attr].isin([v])]
             # if the sample numbers of D_v is zero, set node the class which has the most samples in D
             if df_v.empty:
-                node.attr_down[v] = Node(None, max(label_count, key=label_count.get()), {})
+                node.attr_down[v] = Node(None, max(label_count, key=label_count.get), {})
             # remove optimal attribute and generate tree based on D_v
             else:
                 df_v = df_v.drop(opt_attr, 1)
@@ -170,5 +160,30 @@ def TreeGenerate(df):
     # return tree
     return node
 
+
+def TreeToGraph(root, i, g):
+    if root.attr == None:
+        g_node_label = "Node:%d\n好瓜:%s" % (i, root.label)
+    else:
+        g_node_label = "Node:%d\n好瓜:%s\n属性:%s\n" % (i, root.label, root.attr)
+    g_node = i
+    g.add_node(graphviz.Node(g_node, label=g_node_label))
+
+    for value in list(root.attr_down):
+        i, g_child = TreeToGraph(root.attr_down[value], i+1, g)
+        g.add_edge(graphviz.Edge(g_node, g_child, label=value))
+    return i, g_node
+
+
+def DrawPng(root, out_file):
+    g = graphviz.Dot()
+    TreeToGraph(root, 0, g)
+    g.write("../data/tree.dot")
+
+
 if __name__ == "__main__":
-    loadDataSet("../data/watermelon_3.csv")
+    df = loadDataSet("../data/watermelon_2.csv")
+    index_train = [0, 1, 2, 5, 6, 9, 13, 14, 15, 16]
+    df_train = df.iloc[index_train]
+    root = TreeGenerate(df_train)
+    DrawPng(root, "../data/desicion_tree.png")
